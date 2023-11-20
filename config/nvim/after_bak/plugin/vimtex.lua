f701f9caf5e8eab73c2e3201d3fb80dc0b0152bf
PJK on workstation <522ph1008@nitrkl.ac.in>
2023-11-20 18:42:53 +0530

    Removing the old backup configs since they are no longer needed

--- config/nvim/after_bak/plugin/vimtex.lua
+++
@@ -1,9 +0,0 @@
-vim.g.maplocalleader = '\\'
-vim.g.tex_flavor = 'latex'
-vim.g.vimtex_view_method = 'zathura'
-vim.g.vimtex_quickfix_mode = 1
-vim.g.vimtex_enabled = 1
-vim.g.tex_conceal = 'abdmg'
-vim.g.vimtex_mappings_enabled = 1
-vim.opt.conceallevel = 1
-vim.opt.encoding = 'utf8'
